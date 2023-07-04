12e1448bf1a50dc98aaa0661c35479965d01af4e
Zelin Shen <zshen25@vt.edu>
2023-07-04 16:14:10 -0400

    Update chassis_task.h
    spin speed change

--- application/chassis_task.h
+++ application/chassis_task.h
@@ -149,8 +149,12 @@ SZL 5-21-2022 重新算
 #define SLOW_SPEED 1.7f	//5-24-2022之前 1.4f //低速 模式速度
 #define SLOW_ACC_STEP 0.015f //油门步进（加速度控制）
 #define SLOW_INT_SPEED 0.1f //初始加速值（降低加速迟	滞）
-#define SPIN_SPEED 4.8f//3.8f//3.3f//5-24-2022之前3.6f//6.0f //小陀螺速度
+#define SPIN_SPEED 3.0f //小陀螺速度
 /*
+7-4-2023: 基于之前测试:
+#define SPIN_SPEED 3.0f - 小陀螺时不会消耗太多功率
+#define SPIN_SPEED 4.8f - 快很多
+-------------------------------------------
 原始的是: #define SPIN_SPEED 6.0f //小陀螺速度
 //PR 底盘线性油门
 #define TURBO_SPEED 2.8f //全油门模式速度
